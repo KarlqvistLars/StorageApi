23fc916545a4a30b76f0efcee6426a727ef46021
KarlqvistLars <le.karlqvist@gmail.com>
2026-06-08 14:13:50 +0200

    Add controller-storage

---
+++ StorageApi/Controllers/ProductsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StorageApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+    }
+}
