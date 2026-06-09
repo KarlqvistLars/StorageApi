bd12c2c20c3f3c5bb5c5ef68424fbd49aeab6a96
KarlqvistLars <le.karlqvist@gmail.com>
2026-06-09 17:48:37 +0200

    Stats func tillagt i ProductStatsDto

---
+++ StorageApi/DTOs/ProductStatsDto.cs
@@ -0,0 +1,14 @@
+﻿namespace StorageApi.DTOs
+{
+    public class ProductStatsDto
+    {
+        public ProductStatsDto()
+        {
+        }
+        public int TotalProducts { get; set; }
+        public decimal AveragePrice { get; set; }
+        public decimal TotalValue { get; set; }
+
+        public Dictionary<string, int>? CategoryCounts { get; set; }
+    }
+}
